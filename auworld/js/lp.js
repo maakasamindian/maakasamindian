36437a90191ade2543627e3dcb954664272e5c63
maakasamindian <74304156+maakasamindian@users.noreply.github.com>
2022-01-10 08:05:52 +0530

    Delete auworld directory

--- auworld/js/lp.js
+++
@@ -1,9 +0,0 @@
-
-function pLPCommand(c, a1, a2, a3, a4) {
-parent.window["pLPCommand3"] && parent.window["pLPCommand3"](c, a1, a2, a3, a4);
-}
-function pRTLPCB(pN, data) {
-parent.window["pRTLPCB3"] && parent.window["pRTLPCB3"](pN, data);
-}
-         pLPCommand('start','1161630','fF4f7PPP81');
-pRTLPCB(0,[{"t":"c","d":{"t":"h","d":{"ts":1640266666894,"v":"5","h":"s-usc1c-nss-344.firebaseio.com","s":"gmpeOBMYiFqvhALDFU39TKZgs3ucjF8k"}}}]);
--- auworld/js/lp2.js
+++
@@ -1,9 +0,0 @@
-
-function pLPCommand(c, a1, a2, a3, a4) {
-parent.window["pLPCommand8"] && parent.window["pLPCommand8"](c, a1, a2, a3, a4);
-}
-function pRTLPCB(pN, data) {
-parent.window["pRTLPCB8"] && parent.window["pRTLPCB8"](pN, data);
-}
-         pLPCommand('start','1161612','0Hql7yGKld');
-pRTLPCB(0,[{"t":"c","d":{"t":"h","d":{"ts":1640266658064,"v":"5","h":"s-usc1c-nss-344.firebaseio.com","s":"xjndNIMYQrf086WueL9qf5KD9p2vm0qT"}}}]);
